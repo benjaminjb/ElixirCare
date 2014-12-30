1613e4bfa89f1c6e723579767439315ecd95cd16
Benjamin Blattberg <benjamin.blattberg@gmail.com>
2014-12-30 14:03:12 -0600

    Example test passing

--- test/elixircare_test.exs
+++ test/elixircare_test.exs
@@ -1,7 +1,27 @@
-defmodule ElixircareTest do
-  use ExUnit.Case
+# defmodule ElixircareTest do
+#   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+#   test "the truth" do
+#     assert 1 + 1 == 2
+#   end
+# end
+
+defmodule MyPlugTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+
+  @opts AppRouter.init([])
+
+  test "returns hello world" do
+    # Create a test connection
+    conn = conn(:get, "/hello")
+
+    # Invoke the plug
+    conn = AppRouter.call(conn, @opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "world"
   end
 end
